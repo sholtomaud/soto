d7e7de410faf150ec6350d7de255640e3b37f43b
Sholto Maud <sholto.maud@gmail.com>
2016-01-26 15:07:08 +1000

    up

--- scripts/ui/index.js
+++ scripts/ui/index.js
@@ -1,31 +1,6 @@
 var fastn = require('^fastn');
 
-
-// var y = [0, 380, 414, 580, 720, 768, 990]; //breakpoints. 
-// var w, x = 1, z = y.length;
-
-// function init(){
-//     for ( ; x < z; x++ ) {
-//          w = window.matchMedia ( '(min-width:' + y[x] + 'px)' );
-//          w.addListener (event);
-         
-//     }
-
-//     w = window.innerWidth;
-//     console.log('w: ', w);
-
-//     for(--z; z; z--) {
-//           x = y[z];
-//           if( w <= x) {
-//             // eval('px'+x+'(false)') 
-//         } 
-//     }
-
-// }
-
-
 window.addEventListener('load', function(){
-    // init();
     var controls = require('./controls');
 
     controls.attach().render();
